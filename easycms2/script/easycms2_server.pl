f790f6eff155355efc43c9e7ceaf00ea64361df7
Andreas Marienborg <andreas.marienborg@gmail.com>
2006-12-27 21:49:18 +0000

    added var/lib

--- easycms2/script/easycms2_server.pl
+++ easycms2/script/easycms2_server.pl
@@ -13,6 +13,7 @@ use Pod::Usage;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use lib "$FindBin::Bin/../../Other/dbic-current/lib";
+use lib "$FindBin::Bin/../../var/lib";
 
 my $debug             = 0;
 my $fork              = 0;
