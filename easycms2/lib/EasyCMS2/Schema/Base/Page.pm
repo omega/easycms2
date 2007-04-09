fdc563d734249feeed7e7bf54664e67aab0e4367
Andreas Marienborg <andreas.marienborg@gmail.com>
2007-04-09 19:50:53 +0000

    attempt at fixing thaw

--- easycms2/lib/EasyCMS2/Schema/Base/Page.pm
+++ easycms2/lib/EasyCMS2/Schema/Base/Page.pm
@@ -64,7 +64,7 @@ __PACKAGE__->inflate_column('to_date' => {
 });
 
 __PACKAGE__->inflate_column('extra' => {
-    'inflate' => sub { my $s = shift; return $t ? thaw($s) : {}; },
+    'inflate' => sub { my $s = shift; return $s ? thaw($s) : {}; },
     'deflate' => sub { return freeze(shift); }
 });
 
