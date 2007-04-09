4f01c968be6d9438256858a2cec99ac408f0dcb4
Andreas Marienborg <andreas.marienborg@gmail.com>
2007-04-09 19:21:28 +0000

    added a index-sub to article category type that fetches all pages in the category

--- easycms2/lib/EasyCMS2/CategoryType/Article.pm
+++ easycms2/lib/EasyCMS2/CategoryType/Article.pm
@@ -4,5 +4,12 @@ use Moose;
 
 extends 'EasyCMS2::CategoryType';
 
+override  'index' => sub {
+    my $self = shift;
+    my $hashref = {};
+    $hashref->{'pages'} = $self->row->pages({});
+    return $hashref;
+};
+
 
 1;
