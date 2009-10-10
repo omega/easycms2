02292c44d72831c0cd276e4a6d270f7c3724b3d9
Andreas Marienborg <andreas.marienborg@gmail.com>
2009-10-10 13:18:02 +0200

    Fix extraction of form_fu forms

--- script/easycms2_extract_strings.sh
+++ script/easycms2_extract_strings.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-BASE=lib/EasyCMS2/I18N
 
-find lib root script -type f -not -path '*.svn*' -not -name 'MochiKit.js' \( -name '*.pm' -or -name '*.tt' -or -name '*.js' -or -name '*.yml' \) > filelist.tmp
-mkdir -p $BASE
-for i in 'en' 'no'; do
-    echo "lang: $i"
-    xgettext.pl --files-from=filelist.tmp -o $BASE/$i.po
-done;
 
-rm filelist.tmp
+I18N=lib/EasyCMS2/I18N
+mkdir -p $I18N
+xgettext.pl -v -v -v --output=$I18N/messages.pot \
+   --directory=lib/ --directory=root/ \
+   --plugin 'perl=pm,pl' --plugin 'tt2=tt,js,xml' --plugin 'formfu=yml'
+if [ -f $I18N/messages.pot ];  then
+for i in no en; do
+    echo $i
+       if [ -f $I18N/$i.po ]; then
+           msgmerge --backup=none --update $I18N/$i.po $I18N/messages.pot
+       else
+        msginit --no-translator --input=$I18N/messages.pot --output=$I18N/$i.po --locale=$i
+    fi
+done
+else
+   echo "No strings found during extraction";
+fi
